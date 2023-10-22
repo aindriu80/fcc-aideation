f215daccce35e9a8133a1009f7fece08748ba25b
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2023-10-22 16:12:00 +0100

    Project Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/src/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <div className="text-red-900">Hello World</div>
+      <Button>Click me !</Button>
+    </>
+  )
+}
