e5157000edcc9f00777eab263b9b61eada8bdd48
aindriu80 <aindriu80@gmail.com>
2023-10-23 20:32:14 +0100

    Working on dashboard, some fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
-import { Button } from '@/src/components/ui/button'
-import Image from 'next/image'
+import TypewriterTitle from '@/src/components/TypewriterTitle'
 
 export default function Home() {
   return (
     <>
-      <div className="text-red-900">Hello World</div>
-      <Button>Click me !</Button>
+      <div className="bg-gradient-to-r min-h-screen from-rose-100 to-teal-100 grainy">
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <h1 className="font-semibold text-7xl text-center">
+            AI <span className="text-green-600 font-bold">note taking</span>{' '}
+            assistant
+          </h1>
+          <div className="mt-4"></div>
+          <h2 className="font-semibold text-3xl text-center text-slate-700">
+            <TypewriterTitle />
+          </h2>
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/components/TypewriterTitle.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+const TypewriterTitle = (props: Props) => {
+  return <div>TypewriterTitle</div>
+}
+
+export default TypewriterTitle
