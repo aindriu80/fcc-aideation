94c153bdd67c1e8251ebf9ceba0e9f9f4d98e4a3
aindriu80 <aindriu80@gmail.com>
2023-10-24 13:50:01 +0100

    Completing the landing page

--- src/components/TypewriterTitle.tsx
+++ src/components/TypewriterTitle.tsx
@@ -1,9 +1,23 @@
+'use client'
 import React from 'react'
+import Typewriter from 'typewriter-effect'
 
 type Props = {}
 
 const TypewriterTitle = (props: Props) => {
-  return <div>TypewriterTitle</div>
+  return (
+    <Typewriter
+      options={{ loop: true }}
+      onInit={(typewriter) => {
+        typewriter
+          .typeString('🚀 Supercharged Productivity.')
+          .pauseFor(1000)
+          .deleteAll()
+          .typeString('🤖 AI-Powered Insights.')
+          .start()
+      }}
+    />
+  )
 }
 
 export default TypewriterTitle
