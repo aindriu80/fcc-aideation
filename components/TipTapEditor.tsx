5c105c0abaa3dac20ef8818fff4feb60e8d66399
aindriu80 <aindriu80@gmail.com>
2023-11-01 20:28:16 +0000

    useDebounce to reduce calls to server

--- components/TipTapEditor.tsx
+++ components/TipTapEditor.tsx
@@ -4,6 +4,7 @@ import { useEditor, EditorContent } from '@tiptap/react'
 import { StarterKit } from '@tiptap/starter-kit'
 import TipTapMenuBar from './TipTapMenuBar'
 import { Button } from '@/src/components/ui/button'
+import { useDebounce } from '@/src/lib/useDebounce'
 
 type Props = {}
 
@@ -16,13 +17,16 @@ const TipTapEditor = (props: Props) => {
       setEditorState(editor.getHTML())
     },
   })
+  const debouncedEditorState = useDebounce(editorState, 500)
+  React.useEffect(() => {
+    console.log(debouncedEditorState)
+  }, [editorState])
   return (
     <>
       <div className="flex">
         {editor && <TipTapMenuBar editor={editor} />}
         <Button>Saved</Button>
       </div>
-      :w
       <div className="prose">
         <EditorContent editor={editor} />
       </div>
